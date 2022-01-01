fdf618b85765bdbfb5164ae2f33efafbf3643b6e
lippincott <blippincott@marketactionresearch.com>
2021-12-31 17:05:43 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,12 +13,14 @@ export default function Home() {
       <main>
         <Header title="MARS Landing" />
         <p className="description">
-         Hello! You have reached a sample page by <a href="https://marketactionresearch.com">Market Action Research, Inc.</a>
+         Hello! You have reached simple sample page by <a href="https://marketactionresearch.com">Market Action Research, Inc.</a>
         </p>
+    
+        <p>Market Action Reseaerch is a professional web development, marketing and tech consulting company based in Los Angeles, CA 🏖️ </p>
     
         <h2>What is this?</h2>
          <ul> 
-            <li>This is a placeholder landing page.</li>
+            <li>This is a placeholder landing page that might be 😎 coming soon or 🤔 is no more.</li>
             <li>Need a new website or ecommerce platform? Contact <a href="https://marketactionresearch.com">MARS</a> today!</li>
          </ul>
       
